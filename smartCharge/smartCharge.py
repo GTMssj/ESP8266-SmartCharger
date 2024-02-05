2e02d7e55a88ec55433c17690e6bfff3351a258e
Debian <SteveTao@Steve-Debian.ozrxhyo2vajude0koqzfjzdvzc.hx.internal.cloudapp.net>
2024-02-05 08:10:11 +0000

    modified several changes

--- smartCharge/smartCharge.py
+++ smartCharge/smartCharge.py
@@ -3,6 +3,17 @@ import socket, subprocess, json, time, os
 host = "192.168.1.8"
 port = 4444
 
+####################
+
+#   00  On
+#   01  Off
+
+#   04  Config SSID
+#   05  Config Passwd
+#   06  Config AP on/off
+
+####################
+
 def get_status():
     result = subprocess.run("termux-battery-status", shell=True, capture_output=True, text=True)
     status = json.loads(result.stdout)
@@ -14,14 +25,6 @@ def get_status():
 #    print('Current: '+status["current"])
     print("=== === === ===")
 
-def t_on():
-    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udp_socket.sendto(b'\x00', (host, port))
-    udp_socket.close()
-
-def t_off():
-    send_udp_data(b'\x01', host, port)
-    
 def send_udp_data(data, host, port):
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     udp_socket.sendto(data, (host, port))
@@ -32,9 +35,13 @@ while True:
     get_status()
     match input(' >'):
         case 'on':
-            t_on()
+            send_udp_data(b'\x00', host, port)
+            #t_on()
+            #   0x00    TurnOn
         case 'off':
-            t_off()
+            send_udp_data(b'\x01', host, port)
+            #t_off()
+            #   0x01    TurnOff
         case 'q':
             exit()
 
